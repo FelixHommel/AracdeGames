6b90ca7caf899c0cbd7d64098e0703943908c1c7
Fllixxx <flixh10@gmail.com>
2024-03-17 19:22:22 +0100

    Add project files.

---
+++ Space Invaders/stdafx.h
@@ -0,0 +1,16 @@
+#ifndef STDAFX_H
+#define STDAFX_H
+
+#include "SFML/System.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/Graphics.hpp"
+#include "SFML/Audio.hpp"
+#include "SFML/Network.hpp"
+
+#include <iostream>
+
+class stdafx
+{
+};
+
+#endif // !STDAFX_H
